1da9502c1d23725b5ebcce7080ffb67ac87da350
mng <ngmatt@gmail.com>
2009-10-14 22:38:19 -0700

    Adding baseline for imgtag sub-project

---
+++ imgtag/imgtag.js
@@ -0,0 +1,40 @@
+/**
+ * imgtag.js
+ * 
+ * Image Tagger
+ * Creates an interface to generate "facebook-like" tags to images.
+ * When active, the user click will apply a bordered tag centered around the click.
+ * When inactive, the existing tags will be highlighted when rolled over.
+ * 
+ * @author ngmatt
+ */
+function imgtag() {
+    this.init.apply(this, arguments);
+}
+
+imgtag.prototype = {
+    /**
+     * Initialize image tagger
+     */
+    init: function (config) {
+        
+    },
+    /**
+     * 
+     */
+    addtag: function () {
+      
+    },
+    /**
+     * 
+     */
+    _addlink: function () {
+      
+    },
+    /**
+     * 
+     */
+    _addcssborder: function () {
+      
+    }
+}
